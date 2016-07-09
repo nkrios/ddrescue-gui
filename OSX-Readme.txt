86ba94bbc82e6d2fa791a6aeb808a81f949487ef
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-07-09 11:08:24 +0100

    Added OS X package background, OS X py2app setup files, and OS X package readme.

---
+++ OSX-Readme.txt
@@ -0,0 +1,3 @@
+To install DDRescue-GUI, simply drag and drop DDRescue-GUI in this image to the applications shortcut.
+
+Copyright (C) 2013-2016 Hamish McIntyre-Bhatty
---
+++ setup32.py
@@ -0,0 +1,22 @@
+"""
+This is a setup.py script written for DDRescue-GUI v1.6 (32-bit) 
+by Hamish McIntyre-Bhatty.
+
+Usage:
+    python setup.py py2app
+"""
+
+from setuptools import setup
+
+APP = ['./DDRescue-GUI.py']
+
+DATA_FILES = ['./AuthenticationDialog.py', './ddrescue', './images', './other']
+
+OPTIONS = {'arch': 'i386', 'argv_emulation': True, 'no_strip': True, 'iconfile': './images/Logo.icns', 'includes': 'wx,wx.animate,wx.lib.stattext,wx.lib.statbmp,threading,getopt,logging,time,subprocess,re,os,sys,plistlib', 'packages': 'GetDevInfo,Tools'}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
