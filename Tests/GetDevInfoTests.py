8d58236e6322e0d440865e6992088384a94cb55f
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-01-10 18:01:23 +0000

    Make Mac unit test for DevInfoTools().GetVendor()

--- Tests/GetDevInfoTests.py
+++ Tests/GetDevInfoTests.py
@@ -164,7 +164,7 @@ def ReturnFakeDiskInfoMac():
     DiskInfo["/dev/disk0"]["HostDevice"] = "N/A"
     DiskInfo["/dev/disk0"]["Capacity"] = "200GB"
     DiskInfo["/dev/disk0"]["Type"] = "Device"
-    DiskInfo["/dev/disk0"]["Partitions"] = ["/dev/disk0s1", "/dev/disk0s2"]
+    DiskInfo["/dev/disk0"]["Partitions"] = ["/dev/disk0s1", "/dev/disk0s2", "/dev/disk0s3"]
 
     #Fictional /dev/disk0s1
     DiskInfo["/dev/disk0s1"] = {}
@@ -190,7 +190,556 @@ def ReturnFakeDiskInfoMac():
     DiskInfo["/dev/disk0s2"]["Type"] = "Partition"
     DiskInfo["/dev/disk0s2"]["Partitions"] = []
 
+    #Fictional /dev/disk0s3
+    DiskInfo["/dev/disk0s3"] = {}
+    DiskInfo["/dev/disk0s3"]["Product"] = "Host Device: FakeDisk"
+    DiskInfo["/dev/disk0s3"]["Vendor"] = "FakeOS v3"
+    DiskInfo["/dev/disk0s3"]["Name"] = "/dev/disk0s3"
+    DiskInfo["/dev/disk0s3"]["Description"] = "FAT Volume"
+    DiskInfo["/dev/disk0s3"]["RawCapacity"] = "564313"
+    DiskInfo["/dev/disk0s3"]["HostDevice"] = "/dev/disk0"
+    DiskInfo["/dev/disk0s3"]["Capacity"] = "24.5GB"
+    DiskInfo["/dev/disk0s3"]["Type"] = "Partition"
+    DiskInfo["/dev/disk0s3"]["Partitions"] = []
+
     return DiskInfo
+
+def ReturnFakeDiskutilListPlist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>AllDisks</key>
+	<array>
+		<string>disk0</string>
+		<string>disk0s1</string>
+		<string>disk0s2</string>
+		<string>disk0s3</string>
+	</array>
+	<key>AllDisksAndPartitions</key>
+	<array>
+		<dict>
+			<key>Content</key>
+			<string>GUID_partition_scheme</string>
+			<key>DeviceIdentifier</key>
+			<string>disk0</string>
+			<key>Partitions</key>
+			<array>
+				<dict>
+					<key>Content</key>
+					<string>EFI</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s1</string>
+					<key>DiskUUID</key>
+					<string>A0C85363-E33F-4708-9B6A-68BD0AA062C1</string>
+					<key>Size</key>
+					<integer>209715200</integer>
+					<key>VolumeName</key>
+					<string>EFI</string>
+					<key>VolumeUUID</key>
+					<string>85D67001-D93E-3687-A1C2-79D677F0C2E0</string>
+				</dict>
+				<dict>
+					<key>Content</key>
+					<string>Apple_HFS</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s2</string>
+					<key>DiskUUID</key>
+					<string>72914C17-6469-457F-B2F0-26BE5BD03843</string>
+					<key>MountPoint</key>
+					<string>/</string>
+					<key>Size</key>
+					<integer>42089095168</integer>
+					<key>VolumeName</key>
+					<string>OSX</string>
+					<key>VolumeUUID</key>
+					<string>AC723754-135E-39BE-8A81-C91228501E9B</string>
+				</dict>
+				<dict>
+					<key>Content</key>
+					<string>Apple_Boot</string>
+					<key>DeviceIdentifier</key>
+					<string>disk0s3</string>
+					<key>DiskUUID</key>
+					<string>353F6CFF-8E9C-4480-BC8B-D6357E15299E</string>
+					<key>Size</key>
+					<integer>650002432</integer>
+					<key>VolumeName</key>
+					<string>Recovery HD</string>
+					<key>VolumeUUID</key>
+					<string>F1573C36-EC30-3501-8E0A-E3A424585875</string>
+				</dict>
+			</array>
+			<key>Size</key>
+			<integer>42948853248</integer>
+		</dict>
+	</array>
+	<key>VolumesFromDisks</key>
+	<array>
+		<string>OSX</string>
+	</array>
+	<key>WholeDisks</key>
+	<array>
+		<string>disk0</string>
+	</array>
+</dict>
+</plist>"""
+
+def ReturnFakeDiskutilInfoBadDisk0Plist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Bootable</key>
+	<false/>
+	<key>BusProtocol</key>
+	<string>SATA</string>
+	<key>CanBeMadeBootable</key>
+	<false/>
+	<key>CanBeMadeBootableRequiresDestroy</key>
+	<false/>
+	<key>Content</key>
+	<string>GUID_partition_scheme</string>
+	<key>DeviceBlockSize</key>
+	<integer>512</integer>
+	<key>DeviceIdentifier</key>
+	<string>disk0</string>
+	<key>DeviceNode</key>
+	<string>/dev/disk0</string>
+	<key>DeviceTreePath</key>
+	<string>IODeviceTree:/PCI0@1e0000/pci8086,2829@1F,2/PRT0@0/PMP@0</string>
+	<key>Ejectable</key>
+	<false/>
+	<key>EjectableMediaAutomaticUnderSoftwareControl</key>
+	<false/>
+	<key>EjectableOnly</key>
+	<false/>
+	<key>FreeSpace</key>
+	<integer>0</integer>
+	<key>GlobalPermissionsEnabled</key>
+	<false/>
+	<key>IOKitSize</key>
+	<integer>42948853248</integer>
+	<key>IORegistryEntryName</key>
+	<string>VBOX HARDDISK Media</string>
+	<key>Internal</key>
+	<true/>
+	<key>LowLevelFormatSupported</key>
+	<false/>
+	<key>MediaType</key>
+	<string>Generic</string>
+	<key>MountPoint</key>
+	<string></string>
+	<key>OS9DriversInstalled</key>
+	<false/>
+	<key>ParentWholeDisk</key>
+	<string>disk0</string>
+	<key>RAIDMaster</key>
+	<false/>
+	<key>RAIDSlice</key>
+	<false/>
+	<key>Removable</key>
+	<false/>
+	<key>RemovableMedia</key>
+	<false/>
+	<key>RemovableMediaOrExternalDevice</key>
+	<false/>
+	<key>SMARTStatus</key>
+	<string>Not Supported</string>
+	<key>Size</key>
+	<integer>42948853248</integer>
+	<key>SolidState</key>
+	<false/>
+	<key>SupportsGlobalPermissionsDisable</key>
+	<false/>
+	<key>SystemImage</key>
+	<false/>
+	<key>TotalSize</key>
+	<integer>42948853248</integer>
+	<key>VirtualOrPhysical</key>
+	<string>Physical</string>
+	<key>VolumeName</key>
+	<string></string>
+	<key>VolumeSize</key>
+	<integer>0</integer>
+	<key>WholeDisk</key>
+	<true/>
+	<key>Writable</key>
+	<true/>
+	<key>WritableMedia</key>
+	<true/>
+	<key>WritableVolume</key>
+	<false/>
+</dict>
+</plist>"""
+
+def ReturnFakeDiskutilInfoDisk0Plist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Bootable</key>
+	<false/>
+	<key>BusProtocol</key>
+	<string>SATA</string>
+	<key>CanBeMadeBootable</key>
+	<false/>
+	<key>CanBeMadeBootableRequiresDestroy</key>
+	<false/>
+	<key>Content</key>
+	<string>GUID_partition_scheme</string>
+	<key>DeviceBlockSize</key>
+	<integer>512</integer>
+	<key>DeviceIdentifier</key>
+	<string>disk0</string>
+	<key>DeviceNode</key>
+	<string>/dev/disk0</string>
+	<key>DeviceTreePath</key>
+	<string>IODeviceTree:/PCI0@1e0000/pci8086,2829@1F,2/PRT0@0/PMP@0</string>
+	<key>Ejectable</key>
+	<false/>
+	<key>EjectableMediaAutomaticUnderSoftwareControl</key>
+	<false/>
+	<key>EjectableOnly</key>
+	<false/>
+	<key>FreeSpace</key>
+	<integer>0</integer>
+	<key>GlobalPermissionsEnabled</key>
+	<false/>
+	<key>IOKitSize</key>
+	<integer>42948853248</integer>
+	<key>IORegistryEntryName</key>
+	<string>VBOX HARDDISK Media</string>
+	<key>Internal</key>
+	<true/>
+	<key>LowLevelFormatSupported</key>
+	<false/>
+	<key>MediaName</key>
+	<string>VBOX HARDDISK</string>
+	<key>MediaType</key>
+	<string>Generic</string>
+	<key>MountPoint</key>
+	<string></string>
+	<key>OS9DriversInstalled</key>
+	<false/>
+	<key>ParentWholeDisk</key>
+	<string>disk0</string>
+	<key>RAIDMaster</key>
+	<false/>
+	<key>RAIDSlice</key>
+	<false/>
+	<key>Removable</key>
+	<false/>
+	<key>RemovableMedia</key>
+	<false/>
+	<key>RemovableMediaOrExternalDevice</key>
+	<false/>
+	<key>SMARTStatus</key>
+	<string>Not Supported</string>
+	<key>Size</key>
+	<integer>42948853248</integer>
+	<key>SolidState</key>
+	<false/>
+	<key>SupportsGlobalPermissionsDisable</key>
+	<false/>
+	<key>SystemImage</key>
+	<false/>
+	<key>TotalSize</key>
+	<integer>42948853248</integer>
+	<key>VirtualOrPhysical</key>
+	<string>Physical</string>
+	<key>VolumeName</key>
+	<string></string>
+	<key>VolumeSize</key>
+	<integer>0</integer>
+	<key>WholeDisk</key>
+	<true/>
+	<key>Writable</key>
+	<true/>
+	<key>WritableMedia</key>
+	<true/>
+	<key>WritableVolume</key>
+	<false/>
+</dict>
+</plist>"""
+
+def ReturnFakeDiskutilInfoDisk0s1Plist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Bootable</key>
+	<false/>
+	<key>BusProtocol</key>
+	<string>SATA</string>
+	<key>CanBeMadeBootable</key>
+	<false/>
+	<key>CanBeMadeBootableRequiresDestroy</key>
+	<false/>
+	<key>Content</key>
+	<string>EFI</string>
+	<key>DeviceBlockSize</key>
+	<integer>512</integer>
+	<key>DeviceIdentifier</key>
+	<string>disk0s1</string>
+	<key>DeviceNode</key>
+	<string>/dev/disk0s1</string>
+	<key>DeviceTreePath</key>
+	<string>IODeviceTree:/PCI0@1e0000/pci8086,2829@1F,2/PRT0@0/PMP@0</string>
+	<key>DiskUUID</key>
+	<string>A0C85363-E33F-4708-9B6A-68BD0AA062C1</string>
+	<key>Ejectable</key>
+	<false/>
+	<key>EjectableMediaAutomaticUnderSoftwareControl</key>
+	<false/>
+	<key>EjectableOnly</key>
+	<false/>
+	<key>FreeSpace</key>
+	<integer>0</integer>
+	<key>GlobalPermissionsEnabled</key>
+	<false/>
+	<key>IOKitSize</key>
+	<integer>209715200</integer>
+	<key>IORegistryEntryName</key>
+	<string>EFI System Partition</string>
+	<key>Internal</key>
+	<true/>
+	<key>MediaName</key>
+	<string></string>
+	<key>MediaType</key>
+	<string>Generic</string>
+	<key>MountPoint</key>
+	<string></string>
+	<key>ParentWholeDisk</key>
+	<string>disk0</string>
+	<key>RAIDMaster</key>
+	<false/>
+	<key>RAIDSlice</key>
+	<false/>
+	<key>Removable</key>
+	<false/>
+	<key>RemovableMedia</key>
+	<false/>
+	<key>RemovableMediaOrExternalDevice</key>
+	<false/>
+	<key>SMARTStatus</key>
+	<string>Not Supported</string>
+	<key>Size</key>
+	<integer>209715200</integer>
+	<key>SolidState</key>
+	<false/>
+	<key>SupportsGlobalPermissionsDisable</key>
+	<false/>
+	<key>SystemImage</key>
+	<false/>
+	<key>TotalSize</key>
+	<integer>209715200</integer>
+	<key>VolumeName</key>
+	<string>EFI</string>
+	<key>VolumeSize</key>
+	<integer>0</integer>
+	<key>VolumeUUID</key>
+	<string>85D67001-D93E-3687-A1C2-79D677F0C2E0</string>
+	<key>WholeDisk</key>
+	<false/>
+	<key>Writable</key>
+	<true/>
+	<key>WritableMedia</key>
+	<true/>
+	<key>WritableVolume</key>
+	<false/>
+</dict>
+</plist>"""
+
+def ReturnFakeDiskutilInfoDisk0s2Plist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Bootable</key>
+	<true/>
+	<key>BusProtocol</key>
+	<string>SATA</string>
+	<key>CanBeMadeBootable</key>
+	<false/>
+	<key>CanBeMadeBootableRequiresDestroy</key>
+	<false/>
+	<key>Content</key>
+	<string>Apple_HFS</string>
+	<key>DeviceBlockSize</key>
+	<integer>512</integer>
+	<key>DeviceIdentifier</key>
+	<string>disk0s2</string>
+	<key>DeviceNode</key>
+	<string>/dev/disk0s2</string>
+	<key>DeviceTreePath</key>
+	<string>IODeviceTree:/PCI0@1e0000/pci8086,2829@1F,2/PRT0@0/PMP@0</string>
+	<key>DiskUUID</key>
+	<string>72914C17-6469-457F-B2F0-26BE5BD03843</string>
+	<key>Ejectable</key>
+	<false/>
+	<key>EjectableMediaAutomaticUnderSoftwareControl</key>
+	<false/>
+	<key>EjectableOnly</key>
+	<false/>
+	<key>FilesystemName</key>
+	<string>Journaled HFS+</string>
+	<key>FilesystemType</key>
+	<string>hfs</string>
+	<key>FilesystemUserVisibleName</key>
+	<string>Mac OS Extended (Journaled)</string>
+	<key>FreeSpace</key>
+	<integer>18913898496</integer>
+	<key>GlobalPermissionsEnabled</key>
+	<true/>
+	<key>IOKitSize</key>
+	<integer>42089095168</integer>
+	<key>IORegistryEntryName</key>
+	<string>OSX</string>
+	<key>Internal</key>
+	<true/>
+	<key>JournalOffset</key>
+	<integer>1310720</integer>
+	<key>JournalSize</key>
+	<integer>8388608</integer>
+	<key>MediaName</key>
+	<string></string>
+	<key>MediaType</key>
+	<string>Generic</string>
+	<key>MountPoint</key>
+	<string>/</string>
+	<key>ParentWholeDisk</key>
+	<string>disk0</string>
+	<key>RAIDMaster</key>
+	<false/>
+	<key>RAIDSlice</key>
+	<false/>
+	<key>RecoveryDeviceIdentifier</key>
+	<string>disk0s3</string>
+	<key>Removable</key>
+	<false/>
+	<key>RemovableMedia</key>
+	<false/>
+	<key>RemovableMediaOrExternalDevice</key>
+	<false/>
+	<key>SMARTStatus</key>
+	<string>Not Supported</string>
+	<key>Size</key>
+	<integer>42089095168</integer>
+	<key>SolidState</key>
+	<false/>
+	<key>SupportsGlobalPermissionsDisable</key>
+	<true/>
+	<key>SystemImage</key>
+	<false/>
+	<key>TotalSize</key>
+	<integer>42089095168</integer>
+	<key>VolumeAllocationBlockSize</key>
+	<integer>4096</integer>
+	<key>VolumeName</key>
+	<string>OSX</string>
+	<key>VolumeSize</key>
+	<integer>42089095168</integer>
+	<key>VolumeUUID</key>
+	<string>AC723754-135E-39BE-8A81-C91228501E9B</string>
+	<key>WholeDisk</key>
+	<false/>
+	<key>Writable</key>
+	<true/>
+	<key>WritableMedia</key>
+	<true/>
+	<key>WritableVolume</key>
+	<true/>
+</dict>
+</plist>"""
+
+def ReturnFakeDiskutilInfoDisk0s3Plist():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Bootable</key>
+	<true/>
+	<key>BusProtocol</key>
+	<string>SATA</string>
+	<key>CanBeMadeBootable</key>
+	<false/>
+	<key>CanBeMadeBootableRequiresDestroy</key>
+	<false/>
+	<key>Content</key>
+	<string>Apple_Boot</string>
+	<key>DeviceBlockSize</key>
+	<integer>512</integer>
+	<key>DeviceIdentifier</key>
+	<string>disk0s3</string>
+	<key>DeviceNode</key>
+	<string>/dev/disk0s3</string>
+	<key>DeviceTreePath</key>
+	<string>IODeviceTree:/PCI0@1e0000/pci8086,2829@1F,2/PRT0@0/PMP@0</string>
+	<key>DiskUUID</key>
+	<string>353F6CFF-8E9C-4480-BC8B-D6357E15299E</string>
+	<key>Ejectable</key>
+	<false/>
+	<key>EjectableMediaAutomaticUnderSoftwareControl</key>
+	<false/>
+	<key>EjectableOnly</key>
+	<false/>
+	<key>FreeSpace</key>
+	<integer>0</integer>
+	<key>GlobalPermissionsEnabled</key>
+	<false/>
+	<key>IOKitSize</key>
+	<integer>650002432</integer>
+	<key>IORegistryEntryName</key>
+	<string>Recovery HD</string>
+	<key>Internal</key>
+	<true/>
+	<key>MediaName</key>
+	<string></string>
+	<key>MediaType</key>
+	<string>Generic</string>
+	<key>MountPoint</key>
+	<string></string>
+	<key>ParentWholeDisk</key>
+	<string>disk0</string>
+	<key>RAIDMaster</key>
+	<false/>
+	<key>RAIDSlice</key>
+	<false/>
+	<key>Removable</key>
+	<false/>
+	<key>RemovableMedia</key>
+	<false/>
+	<key>RemovableMediaOrExternalDevice</key>
+	<false/>
+	<key>SMARTStatus</key>
+	<string>Not Supported</string>
+	<key>Size</key>
+	<integer>650002432</integer>
+	<key>SolidState</key>
+	<false/>
+	<key>SupportsGlobalPermissionsDisable</key>
+	<false/>
+	<key>SystemImage</key>
+	<false/>
+	<key>TotalSize</key>
+	<integer>650002432</integer>
+	<key>VolumeName</key>
+	<string>Recovery HD</string>
+	<key>VolumeSize</key>
+	<integer>0</integer>
+	<key>VolumeUUID</key>
+	<string>F1573C36-EC30-3501-8E0A-E3A424585875</string>
+	<key>WholeDisk</key>
+	<false/>
+	<key>Writable</key>
+	<true/>
+	<key>WritableMedia</key>
+	<true/>
+	<key>WritableVolume</key>
+	<false/>
+</dict>
+</plist>"""
 
 class TestIsPartition(unittest.TestCase):
     def setUp(self):
@@ -242,8 +791,12 @@ class TestGetVendorProductCapacityDescription(unittest.TestCase):
             self.BadNode3 = BadNode3().GetCopy()
 
         else:
-            #TODO
-            pass
+            GetDevInfo.getdevinfo.DiskInfo = ReturnFakeDiskInfoMac()
+            self.BadPlist0 = ReturnDiskutilInfoBadDisk0Plist()
+            self.Plist0 = ReturnDiskutilInfoDisk0Plist()
+            self.Plist0s1 = ReturnDiskutilInfoDisk0s1Plist()
+            self.Plist0s2 = ReturnDiskutilInfoDisk0s2Plist()
+            self.Plist0s3 = ReturnDiskutilInfoDisk0s3Plist()
 
     def tearDown(self):
         if Linux:
@@ -254,14 +807,40 @@ class TestGetVendorProductCapacityDescription(unittest.TestCase):
             del self.BadNode3
 
         else:
-            #TODO
-            pass
+            del GetDevInfo.GetDevInfo.DiskInfo
+            del self.BadPlist0
+            del self.Plist0
+            del self.Plist0s1
+            del self.Plist0s2
+            del self.Plist0s3
 
     @unittest.skipUnless(Linux, "Linux-specific test")
     def testGetVendorLinux(self):
         self.assertEqual(DevInfoTools().GetVendor(Node=self.Node1), "FakeVendor")
         self.assertEqual(DevInfoTools().GetVendor(Node=self.Node2), "FakeVendor2")
         self.assertEqual(DevInfoTools().GetVendor(Node=self.BadNode1), "Unknown")
+
+    @unittest.skipUnless(not Linux, "Mac-specific test")
+    def testGetVendorMac(self):
+        #baddisk0
+        GetDevInfo.getdevinfo.Main.Plist = self.BadPlist0
+        self.assertEqual(DevInfoTools().GetVendor(Disk="disk0"), "Unknown")
+
+        #disk0
+        GetDevInfo.getdevinfo.Main.Plist = self.Plist0
+        self.assertEqual(DevInfoTools().GetVendor(Disk="disk0"), "VBOX")
+
+        #disk0s1
+        GetDevInfo.getdevinfo.Main.Plist = self.Plist0s1
+        self.assertEqual(DevInfoTools().GetVendor(Disk="disk0s1"), "VBOX")
+
+        #disk0s2
+        GetDevInfo.getdevinfo.Main.Plist = self.Plist0s2
+        self.assertEqual(DevInfoTools().GetVendor(Disk="disk0s2"), "VBOX")
+
+        #disk0s3
+        GetDevInfo.getdevinfo.Main.Plist = self.Plist0s3
+        self.assertEqual(DevInfoTools().GetVendor(Disk="disk0s3"), "VBOX")
 
     @unittest.skipUnless(Linux, "Linux-specific test")
     def testGetProductLinux(self):
