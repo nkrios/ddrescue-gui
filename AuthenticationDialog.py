54a4a795dbd2f2f19fd78f3fda4ace2d6cc59803
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-01-18 15:04:02 +0000

    Use pylint to tidy up AuthenticationDialog.py

--- AuthenticationDialog.py
+++ AuthenticationDialog.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 # Authentication Dialog for DDRescue-GUI Version 1.8
 # This file is part of DDRescue-GUI.
 # Copyright (C) 2013-2018 Hamish McIntyre-Bhatty
@@ -15,245 +15,279 @@
 # You should have received a copy of the GNU General Public License
 # along with DDRescue-GUI.  If not, see <http://www.gnu.org/licenses/>.
 
-#Do future imports to prepare to support python 3. Use unicode strings rather than ASCII strings, as they fix potential problems.
+"""
+A simple authentication dialog that is used when elevated privileges are required.
+Until version 1.8, this was used to start the GUI, but since that release, privileges
+are only escalated when required to improve security.
+"""
+
+#Do future imports to prepare to support python 3.
+#Use unicode strings rather than ASCII strings, as they fix potential problems.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
 #Import other modules
-import wx
-import wx.animate
 import time
 import subprocess
 import os
 import sys
+import wx
+import wx.animate
 
-#Determine if running on Linux or Mac.
+#Determine if running on LINUX or Mac.
 if "wxGTK" in wx.PlatformInfo:
     #Set the resource path to /usr/share/ddrescue-gui/
-    ResourcePath = '/usr/share/ddrescue-gui'
-    Linux = True
+    RESOURCEPATH = '/usr/share/ddrescue-gui'
+    LINUX = True
 
 elif "wxMac" in wx.PlatformInfo:
     try:
-        #Set the resource path from an environment variable, as mac .apps can be found in various places.
-        ResourcePath = os.environ['RESOURCEPATH']
+        #Set the resource path from an environment variable,
+        #as mac .apps can be found in various places.
+        RESOURCEPATH = os.environ['RESOURCEPATH']
 
     except KeyError:
         #Use '.' as the resource path instead as a fallback.
-        ResourcePath = "."
+        RESOURCEPATH = "."
 
-    Linux = False
+    LINUX = False
 
 #Begin Authentication Window.
-class AuthWindow(wx.Frame):  
+class AuthWindow(wx.Frame): #pylint: disable=too-many-instance-attributes
+    """
+    The main authentication window
+    """
     def __init__(self):
         """Inititalize AuthWindow"""
-        wx.Frame.__init__(self, None, title="DDRescue-GUI - Authenticate", size=(600,400), style=(wx.DEFAULT_FRAME_STYLE | wx.STAY_ON_TOP) ^ (wx.RESIZE_BORDER | wx.MINIMIZE_BOX))
-        self.AuthPanel = wx.Panel(self)
+        wx.Frame.__init__(self, None, title="DDRescue-GUI - Authenticate", size=(600, 400),
+                          style=(wx.DEFAULT_FRAME_STYLE | wx.STAY_ON_TOP) ^ (wx.RESIZE_BORDER | wx.MINIMIZE_BOX))
+
+        self.panel = wx.Panel(self)
 
         #Set the frame's icon.
-        global AppIcon
-        AppIcon = wx.Icon(ResourcePath+"/images/Logo.png", wx.BITMAP_TYPE_PNG)
-        wx.Frame.SetIcon(self, AppIcon)
+        prog_icon = wx.Icon(RESOURCEPATH+"/images/Logo.png", wx.BITMAP_TYPE_PNG)
+        wx.Frame.SetIcon(self, prog_icon)
 
-        self.CreateText()
-        self.CreateButtons()
-        self.CreateOtherWidgets()
-        self.SetupSizers()
-        self.BindEvents()
+        self.create_text()
+        self.create_buttons()
+        self.create_other_widgets()
+        self.setup_sizers()
+        self.bind_events()
 
-        #Call Layout() on self.AuthPanel() to ensure it displays properly.
-        self.AuthPanel.Layout()
+        #Call Layout() on self.panel() to ensure it displays properly.
+        self.panel.Layout()
 
         #Give the password field focus, so the user can start typing immediately.
-        self.PasswordField.SetFocus()
+        self.password_field.SetFocus()
 
-    def CreateText(self):
+    def create_text(self):
         """Create all text for AuthenticationWindow"""
-        self.TitleText = wx.StaticText(self.AuthPanel, -1, "Authentication is required to run DDRescue-GUI.")
-        self.BodyText = wx.StaticText(self.AuthPanel, -1, "DDRescue-GUI requires privileges to run.\nPlease enter your password to grant permission.")
-        self.PasswordText = wx.StaticText(self.AuthPanel, -1, "Password:")
+        self.title_text = wx.StaticText(self.panel, -1,
+                                        "Authentication is required to run DDRescue-GUI.")
+        self.body_text = wx.StaticText(self.panel, -1, "DDRescue-GUI requires privileges "
+                                       + "to run.\nPlease enter your password to grant permission.")
 
-        Font = self.TitleText.GetFont()
-        Font.SetWeight(wx.BOLD)
-        #self.TitleText.SetFont(Font) 
-        self.PasswordText.SetFont(Font)
+        self.password_text = wx.StaticText(self.panel, -1, "password:")
 
-    def CreateButtons(self):
+        bold_font = self.title_text.GetFont()
+        bold_font.SetWeight(wx.BOLD)
+        #self.title_text.Setbold_font(bold_font) NB: Fix for display issues on macOS.
+        self.password_text.SetFont(bold_font)
+
+    def create_buttons(self):
         """Create all buttons for AuthenticationWindow"""
-        self.CancelButton = wx.Button(self.AuthPanel, -1, "Cancel")
-        self.AuthButton = wx.Button(self.AuthPanel, -1, "Authenticate")
+        self.cancel_button = wx.Button(self.panel, -1, "Cancel")
+        self.auth_button = wx.Button(self.panel, -1, "Authenticate")
 
-    def CreateOtherWidgets(self):
+    def create_other_widgets(self):
         """Create all other widgets for AuthenticationWindow"""
         #Create the image.
-        img = wx.Image(ResourcePath+"/images/Logo.png", wx.BITMAP_TYPE_PNG)
-        self.Image = wx.StaticBitmap(self.AuthPanel, -1, wx.BitmapFromImage(img))
+        img = wx.Image(RESOURCEPATH+"/images/Logo.png", wx.BITMAP_TYPE_PNG)
+        self.program_logo = wx.StaticBitmap(self.panel, -1, wx.BitmapFromImage(img))
 
         #Create the password field.
-        self.PasswordField = wx.TextCtrl(self.AuthPanel, -1, "", style=wx.TE_PASSWORD|wx.TE_PROCESS_ENTER)
-        self.PasswordField.SetBackgroundColour((255,255,255))
+        self.password_field = wx.TextCtrl(self.panel, -1, "",
+                                          style=wx.TE_PASSWORD | wx.TE_PROCESS_ENTER)
+
+        self.password_field.SetBackgroundColour((255, 255, 255))
 
         #Create the throbber.
-        self.Busy = wx.animate.Animation(ResourcePath+"/images/Throbber.gif")
-        self.GreenPulse = wx.animate.Animation(ResourcePath+"/images/GreenPulse.gif")
-        self.RedPulse = wx.animate.Animation(ResourcePath+"/images/RedPulse.gif")
+        self.busy = wx.animate.Animation(RESOURCEPATH+"/images/throbber.gif")
+        self.green_pulse = wx.animate.Animation(RESOURCEPATH+"/images/GreenPulse.gif")
+        self.red_pulse = wx.animate.Animation(RESOURCEPATH+"/images/RedPulse.gif")
 
-        self.Throbber = wx.animate.AnimationCtrl(self.AuthPanel, -1, self.GreenPulse)
-        self.Throbber.SetUseWindowBackgroundColour(True)
-        self.Throbber.SetInactiveBitmap(wx.Bitmap(ResourcePath+"/images/ThrobberRest.png", wx.BITMAP_TYPE_PNG))
-        self.Throbber.SetClientSize(wx.Size(30,30))
+        self.throbber = wx.animate.AnimationCtrl(self.panel, -1, self.green_pulse)
+        self.throbber.SetUseWindowBackgroundColour(True)
+        self.throbber.SetInactiveBitmap(wx.Bitmap(RESOURCEPATH+"/images/ThrobberRest.png",
+                                                  wx.BITMAP_TYPE_PNG))
 
-    def SetupSizers(self):
+        self.throbber.SetClientSize(wx.Size(30, 30))
+
+    def setup_sizers(self):
         """Setup sizers for AuthWindow"""
         #Make the main boxsizer.
-        MainSizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
 
         #Make the top sizer.
-        TopSizer = wx.BoxSizer(wx.HORIZONTAL)
+        top_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         #Make the top text sizer.
-        TopTextSizer = wx.BoxSizer(wx.VERTICAL)
+        top_text_sizer = wx.BoxSizer(wx.VERTICAL)
 
         #Add items to the top text sizer.
-        TopTextSizer.Add(self.TitleText, 0, wx.ALIGN_LEFT|wx.EXPAND)
-        TopTextSizer.Add(self.BodyText, 0, wx.TOP|wx.ALIGN_LEFT|wx.EXPAND, 10)
+        top_text_sizer.Add(self.title_text, 0, wx.ALIGN_LEFT|wx.EXPAND)
+        top_text_sizer.Add(self.body_text, 0, wx.TOP|wx.ALIGN_LEFT|wx.EXPAND, 10)
 
         #Add items to the top sizer.
-        TopSizer.Add(self.Image, 0, wx.LEFT|wx.ALIGN_CENTER, 18)
-        TopSizer.Add(TopTextSizer, 1, wx.LEFT|wx.ALIGN_CENTER, 29)
+        top_sizer.Add(self.program_logo, 0, wx.LEFT|wx.ALIGN_CENTER, 18)
+        top_sizer.Add(top_text_sizer, 1, wx.LEFT|wx.ALIGN_CENTER, 29)
 
         #Make the password sizer.
-        PasswordSizer = wx.BoxSizer(wx.HORIZONTAL)
+        password_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         #Add items to the password sizer.
-        PasswordSizer.Add(self.PasswordText, 0, wx.LEFT|wx.ALIGN_CENTER, 12)
-        PasswordSizer.Add(self.PasswordField, 1, wx.LEFT|wx.ALIGN_CENTER, 22)
-        PasswordSizer.Add(self.Throbber, 0, wx.LEFT|wx.ALIGN_CENTER|wx.FIXED_MINSIZE, 10)
+        password_sizer.Add(self.password_text, 0, wx.LEFT|wx.ALIGN_CENTER, 12)
+        password_sizer.Add(self.password_field, 1, wx.LEFT|wx.ALIGN_CENTER, 22)
+        password_sizer.Add(self.throbber, 0, wx.LEFT|wx.ALIGN_CENTER|wx.FIXED_MINSIZE, 10)
 
         #Make the button sizer.
-        ButtonSizer = wx.BoxSizer(wx.HORIZONTAL)
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         #Add items to the button sizer.
-        ButtonSizer.Add(self.CancelButton, 0, wx.ALIGN_CENTER|wx.EXPAND)
-        ButtonSizer.Add(self.AuthButton, 1, wx.LEFT|wx.ALIGN_CENTER|wx.EXPAND, 10)
+        button_sizer.Add(self.cancel_button, 0, wx.ALIGN_CENTER|wx.EXPAND)
+        button_sizer.Add(self.auth_button, 1, wx.LEFT|wx.ALIGN_CENTER|wx.EXPAND, 10)
 
         #Add items to the main sizer.
-        MainSizer.Add(TopSizer, 0, wx.ALL|wx.ALIGN_CENTER|wx.EXPAND, 10)
-        MainSizer.Add(PasswordSizer, 0, wx.BOTTOM|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER|wx.EXPAND, 10)
-        MainSizer.Add(ButtonSizer, 1, wx.BOTTOM|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER|wx.EXPAND, 10)
+        main_sizer.Add(top_sizer, 0, wx.ALL|wx.ALIGN_CENTER|wx.EXPAND, 10)
+        main_sizer.Add(password_sizer, 0, wx.BOTTOM|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER|wx.EXPAND, 10)
+        main_sizer.Add(button_sizer, 1, wx.BOTTOM|wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER|wx.EXPAND, 10)
 
         #Get the sizer set up for the frame.
-        self.AuthPanel.SetSizer(MainSizer)
+        self.panel.SetSizer(main_sizer)
 
-        #Call Layout() on self.AuthPanel() to ensure it displays properly.
-        self.AuthPanel.Layout()
+        #Call Layout() on self.panel() to ensure it displays properly.
+        self.panel.Layout()
 
-        MainSizer.SetSizeHints(self)
+        main_sizer.SetSizeHints(self)
 
-    def BindEvents(self):
+    def bind_events(self):
         """Bind all events for AuthenticationWindow"""
-        self.Bind(wx.EVT_TEXT_ENTER, self.OnAuth, self.PasswordField)
-        self.Bind(wx.EVT_BUTTON, self.OnAuth, self.AuthButton)
-        self.Bind(wx.EVT_BUTTON, self.OnExit, self.CancelButton)
+        self.Bind(wx.EVT_TEXT_ENTER, self.on_auth_attempt, self.password_field)
+        self.Bind(wx.EVT_BUTTON, self.on_auth_attempt, self.auth_button)
+        self.Bind(wx.EVT_BUTTON, self.on_exit, self.cancel_button)
 
-    def OnAuth(self, Event=None):
-        """Check the password is correct, then either warn the user or call self.StartDDRescueGUI()"""
-        #Remove any cached credentials, so we don't create a security problem, or say the password is right when it isn't.
+    def on_auth_attempt(self, event=None): #pylint: disable=unused-argument
+        """
+        Check the password is correct,
+        then either warn the user or call self.start_ddrescuegui().
+        """
+
+        #Remove any cached credentials, so we don't create a security problem,
+        #or say the password is right when it isn't.
         subprocess.Popen("sudo -k", shell=True).wait()
 
         #Check the password is right.
-        Password = self.PasswordField.GetLineText(0)
-        Cmd = subprocess.Popen("LC_ALL=C sudo -S echo 'Authentication Succeeded'", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        password = self.password_field.GetLineText(0)
+        cmd = subprocess.Popen("LC_ALL=C sudo -S echo 'Authentication Succeeded'",
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=True)
 
-        #Send the password to sudo through stdin, to avoid showing the user's password in the system/activity monitor.
-        Cmd.stdin.write(Password+"\n")
-        Cmd.stdin.close()
+        #Send the password to sudo through stdin,
+        #to avoid showing the user's password in the system/activity monitor.
+        cmd.stdin.write(password+"\n")
+        cmd.stdin.close()
 
-        self.Throbber.SetAnimation(self.Busy)
-        self.Throbber.Play()
+        self.throbber.SetAnimation(self.busy)
+        self.throbber.Play()
 
-        while Cmd.poll() == None:
+        while cmd.poll() is None:
             wx.Yield()
             time.sleep(0.04)
 
-        Output = Cmd.stdout.read()
+        output = cmd.stdout.read()
 
-        if "Authentication Succeeded" in Output:
+        if "Authentication Succeeded" in output:
             #Set the password field colour to green and disable the auth button.
-            self.PasswordField.SetBackgroundColour((192,255,192))
-            self.AuthButton.Disable()
+            self.password_field.SetBackgroundColour((192, 255, 192))
+            self.auth_button.Disable()
 
             #Play the green pulse for one second.
-            self.Throbber.SetAnimation(self.GreenPulse)
-            self.Throbber.Play()
-            wx.CallLater(1000, self.Throbber.Stop)
-            wx.CallLater(1100, self.StartDDRescueGUI, Password)
+            self.throbber.SetAnimation(self.green_pulse)
+            self.throbber.Play()
+            wx.CallLater(1000, self.throbber.Stop)
+            wx.CallLater(1100, self.start_ddrescuegui, password)
 
         else:
             #Shake the window
-            XPos, YPos = self.GetPosition()
-            Count = 0
+            x_pos, y_pos = self.GetPosition()
+            count = 0
 
-            while Count <= 6:
-                if Count in [0,2,4,6]:
-                    XPos -= 10
+            while count <= 6:
+                if count % 2 == 0:
+                    x_pos -= 10
+
                 else:
-                    XPos += 10
+                    x_pos += 10
 
                 time.sleep(0.02)
-                self.SetPosition((XPos, YPos))
+                self.SetPosition((x_pos, y_pos))
                 wx.Yield()
-                Count += 1
+                count += 1
 
             #Set the password field colour to pink, and select its text.
-            self.PasswordField.SetBackgroundColour((255,192,192))
-            self.PasswordField.SetSelection(0,-1)
-            self.PasswordField.SetFocus()
+            self.password_field.SetBackgroundColour((255, 192, 192))
+            self.password_field.SetSelection(0, -1)
+            self.password_field.SetFocus()
 
             #Play the red pulse for one second.
-            self.Throbber.SetAnimation(self.RedPulse)
-            self.Throbber.Play()
-            wx.CallLater(1000, self.Throbber.Stop)
+            self.throbber.SetAnimation(self.red_pulse)
+            self.throbber.Play()
+            wx.CallLater(1000, self.throbber.Stop)
 
-    def StartDDRescueGUI(self, Password):
+    def start_ddrescuegui(self, password):
         """Start DDRescue-GUI and exit"""
-        if Linux:
+        if LINUX:
             #XXX Fix for running on Wayland until we get policy kit stuff done.
             try:
                 if os.environ['XDG_SESSION_TYPE'] == "wayland":
                     subprocess.Popen("xhost +si:localuser:root", shell=True).wait()
 
-            except KeyError: pass
+            except KeyError:
+                pass
 
-            Cmd = subprocess.Popen("sudo -SH "+ResourcePath+"/DDRescue-GUI.py", stdin=subprocess.PIPE, stdout=sys.stdout, stderr=subprocess.PIPE, shell=True)
+            cmd = subprocess.Popen("sudo -SH "+RESOURCEPATH+"/DDRescue-GUI.py",
+                                   stdin=subprocess.PIPE, stdout=sys.stdout,
+                                   stderr=subprocess.PIPE, shell=True)
 
         else:
-            Cmd = subprocess.Popen("sudo -SH "+ResourcePath+"/../MacOS/DDRescue-GUI", stdin=subprocess.PIPE, stdout=sys.stdout, stderr=subprocess.PIPE, shell=True)
+            cmd = subprocess.Popen("sudo -SH "+RESOURCEPATH+"/../MacOS/DDRescue-GUI",
+                                   stdin=subprocess.PIPE, stdout=sys.stdout,
+                                   stderr=subprocess.PIPE, shell=True)
 
-        #Send the password to sudo through stdin, to avoid showing the user's password in the system/activity monitor.
-        Cmd.stdin.write(Password+"\n")
-        Cmd.stdin.close()
+        #Send the password to sudo through stdin,
+        #to avoid showing the user's password in the system/activity monitor.
+        cmd.stdin.write(password+"\n")
+        cmd.stdin.close()
 
         #Remove any cached credentials, so we don't create a security problem.
         subprocess.Popen("sudo -k", shell=True).wait()
 
-        #Overwrite the password with a string of nonsense characters before deleting it, so the password cannot be read from memory when this script closes.
-        Password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!£$%^&*()_+"
-        del Password
+        #Overwrite the password with a string of nonsense characters before deleting it,
+        #so the password cannot be read from memory when this script closes.
+        password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!£$%^&*()_+"
+        del password
         time.sleep(1)
-        self.OnExit()
+        self.on_exit()
 
-    def OnExit(self, Event=None):
+    def on_exit(self, event=None): #pylint: disable=unused-argument
         """Close AuthWindow() and exit"""
         self.Destroy()
 
 #End Authentication Window.
 
 if __name__ == "__main__":
-    app = wx.App(False)
-    MainFrame = AuthWindow()
-    MainFrame.Show()
-    app.MainLoop()
+    APP = wx.App(False)
+    AuthWindow().Show()
+    APP.MainLoop()
