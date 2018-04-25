1ec24973b3ac3d1925237c477f832ad8cd1b59ec
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-04-25 14:50:35 +0100

    Use resourcepath on macos in runasroot if possible

--- Tools/runasroot.sh
+++ Tools/runasroot.sh
@@ -44,7 +44,7 @@ case "$(ps aux | grep -i DDRescue-GUI.py)" in
             #Start the authentication dialog I wrote.
             resources="."
 
-            if [[ ! $RESOURCEPATH -eq "" ]]; then
+            if [[ ! -z $RESOURCEPATH ]]; then
                 resources=$RESOURCEPATH
             fi
 
