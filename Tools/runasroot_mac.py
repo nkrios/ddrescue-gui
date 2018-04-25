7e93724bbfa8c42743cc13c0fd431757202c3b0f
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-04-25 16:11:52 +0100

    Use the correct return code when returning from runasroot_mac.py

--- Tools/runasroot_mac.py
+++ Tools/runasroot_mac.py
@@ -240,7 +240,7 @@ class AuthWindow(wx.Frame): #pylint: disable=too-many-instance-attributes
         """Start DDRescue-GUI and exit"""
         cmd = subprocess.Popen("sudo -SH sh -c '"+' '.join(sys.argv[1:])+" 2>&1'",
                                stdin=subprocess.PIPE, stdout=sys.stdout,
-                               stderr=subprocess.PIPE, shell=True)
+                               stderr=subprocess.PIPE, shell=True).wait()
 
         #Send the password to sudo through stdin,
         #to avoid showing the user's password in the system/activity monitor.
@@ -254,7 +254,12 @@ class AuthWindow(wx.Frame): #pylint: disable=too-many-instance-attributes
         #so the password cannot be read from memory when this script closes.
         password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!£$%^&*()_+"
         del password
-        time.sleep(1)
+
+        cmd.communicate()
+
+        global returncode
+        returncode = cmd.returncode
+
         self.on_exit()
 
     def on_exit(self, event=None): #pylint: disable=unused-argument
@@ -267,3 +272,4 @@ if __name__ == "__main__":
     APP = wx.App(False)
     AuthWindow().Show()
     APP.MainLoop()
+    sys.exit(returncode)
