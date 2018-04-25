50186595d132ad1a24c66004826d0125fa0017be
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-04-25 14:45:23 +0100

    Use resourcepath on macos in runasroot if possible

--- Tools/runasroot.sh
+++ Tools/runasroot.sh
@@ -42,7 +42,13 @@ case "$(ps aux | grep -i DDRescue-GUI.py)" in
 
         Darwin)
             #Start the authentication dialog I wrote.
-            python3 runasroot_mac.py $@
+            resources=./
+
+            if [[ ! $RESOURCEPATH -eq "" ]]
+                resources=$RESOURCEPATH
+            fi
+
+            python3 $recources+/runasroot_mac.py $@
             ;;
 
         esac
