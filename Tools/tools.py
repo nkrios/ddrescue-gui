1b979155b32aef701a2adfeaac66d8f6ce5a6cc8
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-05-30 15:53:31 +0100

    Attempt to fix using the macOS auth dialog from a non-main thread

--- Tools/tools.py
+++ Tools/tools.py
@@ -303,6 +303,8 @@ class AuthWindow(wx.Frame): #pylint: disable=too-many-instance-attributes
     def run(): #FIXME later
         #Use cached credentials rather than open the auth window if possible.
         if AuthWindow.test_auth():
+            global auth_dialog_open
+            auth_dialog_open = False
             return
 
         global auth_dialog_open
